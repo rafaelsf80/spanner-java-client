7553ef969355c1dd872c9e0da77d6750d16d5757
rafaelsf80 <rafaelsf80@gmail.com>
2017-11-27 12:51:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Google Cloud Java Client for Spanner
+
+Java client for [Cloud Spanner][cloud-spanner].
+
+[![Build Status](https://travis-ci.org/GoogleCloudPlatform/google-cloud-java.svg?branch=master)](https://travis-ci.org/GoogleCloudPlatform/google-cloud-java)
+[![Coverage Status](https://coveralls.io/repos/GoogleCloudPlatform/google-cloud-java/badge.svg?branch=master)](https://coveralls.io/r/GoogleCloudPlatform/google-cloud-java?branch=master)
+[![Maven](https://img.shields.io/maven-central/v/com.google.cloud/google-cloud-spanner.svg)](https://img.shields.io/maven-central/v/com.google.cloud/google-cloud-spanner.svg)
+[![Codacy Badge](https://api.codacy.com/project/badge/grade/9da006ad7c3a4fe1abd142e77c003917)](https://www.codacy.com/app/mziccard/google-cloud-java)
+[![Dependency Status](https://www.versioneye.com/user/projects/58fe4c8d6ac171426c414772/badge.svg?style=flat)](https://www.versioneye.com/user/projects/58fe4c8d6ac171426c414772)
+
+> Note: This client is a work-in-progress
+
+## About Cloud Spanner
+
+[Cloud Spanner][cloud-spanner] is a fully managed, mission-critical relational database service
+built from the ground up and battle tested for transactional consistency, high
+availability, and global scale. With traditional relational semantics (schemas,
+ACID transactions, SQL) and automatic, synchronous replication for high
+availability, Cloud Spanner is the only database service of its kind on the
+market.
+
+Be sure to activate the Cloud Spanner API on the Developer's Console to
+use Cloud Spanner from your project.
+
+See the [Spanner client lib docs][spanner-client-lib-docs] and the [Product Documentation][spanner-product-docs] to learn how to
+interact with Cloud Spanner using this Client Library.
+
+
+## Authentication
+
+See the
+[Authentication](https://github.com/GoogleCloudPlatform/google-cloud-java#authentication)
+section in the base directory's README.
+
+
+## License
+
+Apache 2.0 - See [LICENSE] for more information.
+
+[cloud-spanner]: https://cloud.google.com/spanner/
+[spanner-product-docs]: https://cloud.google.com/spanner/docs/
+[spanner-client-lib-docs]: https://googlecloudplatform.github.io/google-cloud-java/latest/apidocs/index.html?com/google/cloud/spanner/package-summary.html
---
+++ src/com/google/cloud/iberia/SpannerPlayground.java
@@ -0,0 +1,149 @@
+package com.google.cloud.iberia;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.codec.binary.Hex;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.spanner.DatabaseAdminClient;
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.ReadOnlyTransaction;
+import com.google.cloud.spanner.ResultSet;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.TimestampBound;
+import com.google.cloud.spanner.TransactionContext;
+import com.google.cloud.spanner.TransactionRunner.TransactionCallable;
+
+public class SpannerPlayground {
+	
+	public static void main(String[] args) throws Exception {
+		try {
+			GoogleCredentials.getApplicationDefault();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		SpannerOptions options = SpannerOptions.newBuilder().build();
+		Spanner spanner = options.getService();
+		String instanceId = "spanner-demo-rafa";
+		String databaseId = "demo";
+
+		try {
+			SpannerPlayground spannerPlayground = new SpannerPlayground();
+			//spannerPlayground.createDB(spanner, options.getProjectId(), instanceId, databaseId);
+			//spannerPlayground.insertData(spanner, options.getProjectId(), instanceId, databaseId);
+			spannerPlayground.readOnlyFunc(spanner, options.getProjectId(), instanceId, databaseId);
+			//spannerPlayground.readWriteTXN(spanner, options.getProjectId(), instanceId, databaseId);
+		} finally {
+			spanner.close();
+		}
+		System.out.println("Closed client");
+	}
+
+	public void readOnlyFunc(Spanner spanner, String projectId, String instanceId, String databaseId) {
+		DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
+
+		ReadOnlyTransaction txn = dbClient.singleUseReadOnlyTransaction(TimestampBound.ofMaxStaleness(15, TimeUnit.SECONDS));		
+		ResultSet resultSet = txn.executeQuery(
+				Statement.newBuilder(
+						"SELECT a.Name, a.Email FROM Account a, Address ad " +
+						"WHERE a.AccountId = ad.AccountId AND ad.CountryCode = @country LIMIT 10")
+				.bind("country").to("PT").build());
+		while (resultSet.next()) {
+			System.out.printf("%s %s\n", resultSet.getString(0), resultSet.getString(1));
+		}
+	}
+
+	public void readWriteTXN(Spanner spanner, String projectId, String instanceId, String databaseId) {
+		DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
+
+		dbClient.readWriteTransaction().run(
+				new TransactionCallable<Void>() {
+					@Nullable
+					@Override
+					public Void run(TransactionContext txn) throws Exception {
+						ResultSet resultSet = txn.executeQuery(
+								Statement.newBuilder(
+										"SELECT * FROM Account WHERE email=@email")
+								.bind("email").to("myname@mydomain.com")
+								.build());
+						List<Mutation> mutations = new ArrayList<>();
+						while (resultSet.next()) {
+							mutations.add(Mutation.newUpdateBuilder("Account")
+								.set("AccountId").to(resultSet.getString("AccountId"))
+								.set("Name").to("SpannerGuru")
+								.build());
+						}
+						txn.buffer(mutations);
+						return null;
+					}
+				});
+	}
+
+	public void insertData(Spanner spanner, String projectId, String instanceId, String databaseId) {
+		DatabaseClient dbClient = spanner.getDatabaseClient(DatabaseId.of(projectId, instanceId, databaseId));
+
+		String aID = getBase64EncodedUUID();
+		List<Mutation> mutations = new ArrayList<>();
+		mutations.add(Mutation.newInsertBuilder("Account")
+				.set("AccountId").to(aID)
+				.set("Name").to("Robert")
+				.set("Email").to("myname@mydomain.com")
+				.build());
+		mutations.add(Mutation.newInsertBuilder("Address")
+				.set("AccountId").to(aID)
+				.set("AddressId").to(getBase64EncodedUUID())
+				.set("Street").to("My address")
+				.set("City").to("Lisboa")
+				.set("ZIP").to("28020")
+				.set("CountryCode").to("PT")
+				.build());
+		// This writes all the mutations to Cloud Spanner atomically. Get a timestamp, but I skip it
+		dbClient.write(mutations);
+	}
+
+
+	public void createDB(Spanner spanner, String projectId, String instanceId, String databaseId) {
+		DatabaseAdminClient adminClient = spanner.getDatabaseAdminClient();
+		
+		adminClient.createDatabase(instanceId, databaseId, 
+				Arrays.asList(
+						"CREATE TABLE Account (\n"
+								+ "  AccountId  STRING(32) NOT NULL,\n"
+								+ "  Name       STRING(256) NOT NULL,\n"
+								+ "  Email      STRING(256) NOT NULL,\n"
+								+ ") PRIMARY KEY (AccountId)",
+								"CREATE TABLE Address (\n"
+										+ "  AccountId   STRING(32) NOT NULL,\n"
+										+ "  AddressId   STRING(32) NOT NULL,\n"
+										+ "  Street      STRING(1024) NOT NULL,\n"
+										+ "  City        STRING(256) NOT NULL,\n"
+										+ "  ZIP         STRING(20) NOT NULL,\n"
+										+ "  CountryCode STRING(2) NOT NULL,\n"
+										+ ") PRIMARY KEY (AccountId, AddressId),\n"
+										+ "  INTERLEAVE IN PARENT Account ON DELETE CASCADE",
+						"CREATE INDEX ByCountry ON Address(CountryCode) STORING (City, ZIP)")).waitFor();
+		System.out.println("Created database [" + databaseId + "]");
+	}
+
+	public static String getBase64EncodedUUID() {
+		UUID uuid = UUID.randomUUID();
+		ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+		bb.putLong(uuid.getMostSignificantBits());
+		bb.putLong(uuid.getLeastSignificantBits());
+		return new String(Hex.encodeHex(bb.array()));
+	}
+}
